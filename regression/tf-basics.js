87fc09ca1ed40e9eaf1fa7bf3f500e3a5d622283
Carlos Azaustre <cazaustre@gmail.com>
2019-07-07 16:39:03 +0200

    tensorflow basics (2)

--- regression/tf-basics.js
+++ regression/tf-basics.js
@@ -8,15 +8,23 @@ const ys = tf.tensor([2, 2, 2, 2, 2], [5, 1]);
 const res = ys.mul(x).mean();
 res.print();
 
-const optimiser = tf.train.sgd(0.001);
+// Learnin rate is important. 
+// small: (0.00001) slow rate and accurate
+// large: (0.5) you maybe wrong.
+const optimiser = tf.train.sgd(0.005);
 console.log(x.dataSync());
-optimiser.minimize(() => {
-  return ys
-    .mul(x)
-    .square()
-    .mean()
+
+tf.tidy(() => { // to avoid memory leaks
+  for (let i = 0; i < 1000; i++) {
+    optimiser.minimize(() => {
+      return ys
+        .mul(x)
+        .square()
+        .mean()
+    });
+    console.log(x.dataSync());
+  }
 });
-console.log(x.dataSync());
 
 console.log(res);
 console.log(res.dataSync());
